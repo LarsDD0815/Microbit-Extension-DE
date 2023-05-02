7c88cd39eedca77265b45ddc29737039124b6fd2
LarsDD0815 <knuff.oktan.0q@icloud.com>
2023-05-02 16:01:43 +0200

    Aufräumen & Auto-Grundfunktionen ergänzen

--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@ enum LT {
     Center,
     //% block="rechts"
     Right
-    
+
 }
 enum LED {
     //% block="links"
@@ -31,13 +31,60 @@ enum LedState {
 //% groups="['Motor', 'Servo', 'led', 'Neo-pixel', 'Sensor', 'Tone']"
 namespace mecanumRobotV2 {
 
-    const STC15_ADDRESS = 0x30;   //device address
-
-    function i2cWrite(STC15_ADDRESS: number, reg: number, value: number) {
+    function i2cWrite(reg: number, value: number) {
         let buf = pins.createBuffer(2)
         buf[0] = reg
         buf[1] = value
-        pins.i2cWriteBuffer(STC15_ADDRESS, buf)
+        pins.i2cWriteBuffer(0x30, buf)
+    }
+
+    /**
+     * Motor Geschwindigkeit einstellen
+     */
+    //% block="Vorwörts fahren mit Geschwindigkeit: $speed \\%"
+    //% speed.min=0 speed.max=100
+    //% group="Motor" weight=100
+    export function MotorenVorwärts(speed: number) {
+
+        MotorVorneLinks(MD.Forward, speed);
+        MotorVorneRechts(MD.Forward, speed);
+        MotorHintenLinks(MD.Forward, speed);
+        MotorHintenRechts(MD.Forward, speed);
+    }
+
+    /**
+     * Motor Geschwindigkeit einstellen
+     */
+    //% block="Rückwärts fahren mit Geschwindigkeit: $speed \\%"
+    //% speed.min=0 speed.max=100
+    //% group="Motor" weight=99
+    export function MotorenRückwärts(speed: number) {
+
+        MotorVorneLinks(MD.Back, speed);
+        MotorVorneRechts(MD.Back, speed);
+        MotorHintenLinks(MD.Back, speed);
+        MotorHintenRechts(MD.Back, speed);
+    }
+
+    /**
+     * Motor Geschwindigkeit einstellen
+     */
+    //% block="anhalten"
+    //% group="Motor" weight=98
+    export function state() {
+        //if (!PCA9685_Initialized) {
+        //init_PCA9685();
+        //}
+
+        //stop
+        i2cWrite(0x01, 0); //M1A
+        i2cWrite(0x02, 0); //M1B
+        i2cWrite(0x03, 0); //M1A
+        i2cWrite(0x04, 0); //M1B
+        i2cWrite(0x05, 0); //M1A
+        i2cWrite(0x06, 0); //M1B
+        i2cWrite(0x07, 0); //M1A
+        i2cWrite(0x08, 0); //M1B
     }
 
     /**
@@ -46,15 +93,15 @@ namespace mecanumRobotV2 {
     //% block="Vorne rechts $D mit Geschwindigkeit: $speed \\%"
     //% speed.min=0 speed.max=100
     //% group="Motor" weight=97
-    export function MotorVorneRechts(M: LR, D: MD, speed: number) {
+    export function MotorVorneRechts(D: MD, speed: number) {
 
         let speed_value = Math.map(speed, 0, 100, 0, 255);
         if (D == 0) {
-            i2cWrite(0x30, 0x01, 0); //M2A
-            i2cWrite(0x30, 0x02, speed_value); //M2B
+            i2cWrite(0x01, 0); //M2A
+            i2cWrite(0x02, speed_value); //M2B
         } else if (D == 1) {
-            i2cWrite(0x30, 0x01, speed_value); //M2A
-            i2cWrite(0x30, 0x02, 0); //M2B
+            i2cWrite(0x01, speed_value); //M2A
+            i2cWrite(0x02, 0); //M2B
         }
     }
 
@@ -64,15 +111,15 @@ namespace mecanumRobotV2 {
     //% block="Vorne links $D mit Geschwindigkeit: $speed \\%"
     //% speed.min=0 speed.max=100
     //% group="Motor" weight=97
-    export function MotorVorneLinks(M: LR, D: MD, speed: number) {
+    export function MotorVorneLinks(D: MD, speed: number) {
 
         let speed_value = Math.map(speed, 0, 100, 0, 255);
         if (D == 0) {
-            i2cWrite(0x30, 0x03, 0); //M2A
-            i2cWrite(0x30, 0x04, speed_value); //M2B
+            i2cWrite(0x03, 0); //M2A
+            i2cWrite(0x04, speed_value); //M2B
         } else if (D == 1) {
-            i2cWrite(0x30, 0x03, speed_value); //M2A
-            i2cWrite(0x30, 0x04, 0); //M2B
+            i2cWrite(0x03, speed_value); //M2A
+            i2cWrite(0x04, 0); //M2B
         }
     }
 
@@ -82,15 +129,15 @@ namespace mecanumRobotV2 {
     //% block="Hinten links $D mit Geschwindigkeit: $speed \\%"
     //% speed.min=0 speed.max=100
     //% group="Motor" weight=97
-    export function MotorHintenLinks(M: LR, D: MD, speed: number) {
+    export function MotorHintenLinks(D: MD, speed: number) {
 
         let speed_value = Math.map(speed, 0, 100, 0, 255);
         if (D == 0) {
-            i2cWrite(0x30, 0x07, 0); //M2A
-            i2cWrite(0x30, 0x08, speed_value); //M2B
+            i2cWrite(0x07, 0); //M2A
+            i2cWrite(0x08, speed_value); //M2B
         } else if (D == 1) {
-            i2cWrite(0x30, 0x07, speed_value); //M2A
-            i2cWrite(0x30, 0x08, 0); //M2B
+            i2cWrite(0x07, speed_value); //M2A
+            i2cWrite(0x08, 0); //M2B
         }
     }
 
@@ -100,37 +147,16 @@ namespace mecanumRobotV2 {
     //% block="Hinten rechts $D mit Geschwindigkeit: $speed \\%"
     //% speed.min=0 speed.max=100
     //% group="Motor" weight=97
-    export function MotorHintenRechts(M: LR, D: MD, speed: number) {
+    export function MotorHintenRechts(D: MD, speed: number) {
 
         let speed_value = Math.map(speed, 0, 100, 0, 255);
         if (D == 0) {
-            i2cWrite(0x30, 0x05, 0); //M2A
-            i2cWrite(0x30, 0x06, speed_value); //M2B
+            i2cWrite(0x05, 0); //M2A
+            i2cWrite(0x06, speed_value); //M2B
         } else if (D == 1) {
-            i2cWrite(0x30, 0x05, speed_value); //M2A
-            i2cWrite(0x30, 0x06, 0); //M2B
+            i2cWrite(0x05, speed_value); //M2A
+            i2cWrite(0x06, 0); //M2B
         }
-    }
-
-    /**
-     * Motor stoppen
-     */
-    //% block="Auto anhalten"
-    //% group="Motor" weight=98
-    export function state() {
-        //if (!PCA9685_Initialized) {
-        //init_PCA9685();
-        //}
-
-        //stop
-        i2cWrite(0x30, 0x01, 0); //M1A
-        i2cWrite(0x30, 0x02, 0); //M1B
-        i2cWrite(0x30, 0x03, 0); //M1A
-        i2cWrite(0x30, 0x04, 0); //M1B
-        i2cWrite(0x30, 0x05, 0); //M1A
-        i2cWrite(0x30, 0x06, 0); //M1B
-        i2cWrite(0x30, 0x07, 0); //M1A
-        i2cWrite(0x30, 0x08, 0); //M1B
     }
 
     /**
@@ -139,7 +165,7 @@ namespace mecanumRobotV2 {
     //% block="LED $LED $LedS"
     //% group="LED" weight=76
     export function setLed(LED: LED, LedS: LedState) {
-        i2cWrite(0x30, LED, LedS);
+        i2cWrite(LED, LedS);
     }
 
     /**
