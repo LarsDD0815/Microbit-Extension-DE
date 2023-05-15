ddb195a3f1b94b8a9571da41ca2279d4564cbed2
LarsDD0815 <knuff.oktan.0q@icloud.com>
2023-05-15 18:25:31 +0200

    sdf

--- main.ts
+++ main.ts
@@ -37,47 +37,38 @@ namespace mecanumRobotV2 {
         pins.i2cWriteBuffer(0x30, buf)
     }
 
-    function i2cWrite2(reg: number, value: number, repeat: boolean) {
-        let buf = pins.createBuffer(2)
-        buf[0] = reg
-        buf[1] = value
-        pins.i2cWriteBuffer(0x30, buf, repeat)
-    }
-
     //% block="Motoren per Bluetooth steuern: $bluetoothUARTWerte"
     //% group="Motor" weight=95
-    export function stelleMotorenPerBluetooth(bluetoothUARTWerte: string) {
+    export function stelleMotorenPerBluetooth(bluetoothUARTWerte: String) {
 
         let rohdaten = bluetoothUARTWerte.split("|");
         
-        serial.writeLine(bluetoothUARTWerte)
+        let motorVorneRechts = parseInt(rohdaten[0]);
+        let motorVorneLinks = parseInt(rohdaten[1]);
+        let motorHintenRechts = parseInt(rohdaten[2]);
+        let motorHintenLinks = parseInt(rohdaten[3]);
 
-        let motorVorneRechts = parseInt(rohdaten[0], 10);
-        let motorVorneLinks = parseInt(rohdaten[1], 10);
-        let motorHintenRechts = parseInt(rohdaten[2], 10);
-        let motorHintenLinks = parseInt(rohdaten[3], 10);
-
-        stelleMotor(0x01, 0x02, motorVorneRechts, true);
-        stelleMotor(0x03, 0x04, motorVorneLinks, true);
-        stelleMotor(0x05, 0x06, motorHintenRechts, true);
-        stelleMotor(0x07, 0x08, motorHintenLinks, false);
+        stelleMotor(0x01, 0x02, motorVorneRechts);
+        stelleMotor(0x03, 0x04, motorVorneLinks);
+        stelleMotor(0x05, 0x06, motorHintenRechts);
+        stelleMotor(0x07, 0x08, motorHintenLinks);
     }
 
-    function stelleMotor(adresse1: number, adresse2: number, motorwert: number, repeat: boolean) {
+    function stelleMotor(adresse1: number, adresse2: number, motorwert: number) {
         
         let speed = Math.map(Math.abs(motorwert), 0, 100, 0, 254);
 
         serial.writeLine("" + adresse1 + " " +adresse2 + " " + motorwert + " " + speed)
 
         if (motorwert == 0) {
-            i2cWrite2(adresse1, 0, repeat);
-            i2cWrite2(adresse2, 0, repeat);
+            serial.writeLine("" + adresse1 + " " + adresse2 + i2cWrite(adresse1, 0));
+            serial.writeLine("" + adresse1 + " " + adresse2 + i2cWrite(adresse2, 0));
         } else if (motorwert > 0) {
-            i2cWrite2(adresse1, 0, repeat);
-            i2cWrite2(adresse2, speed, repeat);
+            serial.writeLine("" + adresse1 + " " + adresse2 + i2cWrite(adresse1, 0));
+            serial.writeLine("" + adresse1 + " " + adresse2 + i2cWrite(adresse2, speed));
         } else {
-            i2cWrite2(adresse1, speed, repeat);
-            i2cWrite2(adresse2, 0, repeat);
+            serial.writeLine("" + adresse1 + " " + adresse2 + i2cWrite(adresse1, speed));
+            serial.writeLine("" + adresse1 + " " + adresse2 + i2cWrite(adresse2, 0));
         }
     }
 
