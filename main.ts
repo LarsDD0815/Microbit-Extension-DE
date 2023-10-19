8a33902ef09fa9ca9f6a4ab17df446407e3b794a
LarsDD0815 <knuff.oktan.0q@icloud.com>
2023-10-19 20:47:45 +0200

    test

--- main.ts
+++ main.ts
@@ -225,7 +225,7 @@ namespace mecanumRobotV2 {
             const current = aktuelleKompassausrichtung();
 
             if (Math.abs(current - rightTargetAngle) > targetAngleThreshold) {
-                serial.writeLine(aktuelleKompassausrichtung() )
+                serial.writeLine("" + current);
                 continue;
             }
 
