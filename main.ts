1ff10f4454358c898afd34ef2a944340dae3b4f6
LarsDD0815 <knuff.oktan.0q@icloud.com>
2023-10-20 23:21:34 +0200

    test

--- main.ts
+++ main.ts
@@ -102,7 +102,7 @@ namespace mecanumRobotV2 {
         
         const recentAngles: number[] = [];   
         
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 100; i++) {
             recentAngles[i] = input.compassHeading()
             control.waitMicros(10)
         }
