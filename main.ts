d2f5cd812794084e01c0fe0abf33a458e44da2a7
LarsDD0815 <knuff.oktan.0q@icloud.com>
2024-06-16 00:02:27 +0200

    r

--- main.ts
+++ main.ts
@@ -153,9 +153,9 @@ namespace Robotter {
         let wertRegister1 = 0;
         let wertRegister2 = 0;
         if (geschwindigkeit > 0) {
-            wertRegister2 = Math.trunc(Math.map(Math.abs(geschwindigkeit), 1, 100, 32, 255))
+         //   wertRegister2 = Math.trunc(Math.map(Math.abs(geschwindigkeit), 1, 100, 32, 255))
         } else if (geschwindigkeit < 0) {
-            wertRegister1 = Math.trunc(Math.map(Math.abs(geschwindigkeit), 1, 100, 32, 255))
+           // wertRegister1 = Math.trunc(Math.map(Math.abs(geschwindigkeit), 1, 100, 32, 255))
         }
 
         // const buf1 = pins.createBuffer(2)
