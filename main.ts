daa0cf3516d717a526c4cebe5f46c8cb6af8617e
LarsDD0815 <knuff.oktan.0q@icloud.com>
2024-06-15 22:10:10 +0200

    vereinfachung

--- main.ts
+++ main.ts
@@ -10,31 +10,10 @@ namespace Robotter {
             values[i] = valueComputeFunction();
         }
 
-        const valuesWithoutOutliers = filterOutliers(values)
-
         let sumOfValues = 0;
 
-        valuesWithoutOutliers.forEach(function (value: number) {
-            sumOfValues += value;
-        });
+        values.forEach((value: number) => sumOfValues += value);
 
-
-        return Math.round(sumOfValues / valuesWithoutOutliers.length);
-    }
-
-    const filterOutliers = (initialArray: number[]): number[] => {
-        let values, q1, q3, iqr, maxValue: number, minValue: number;
-        values = initialArray.slice().sort((a, b) => a - b);//copy array fast and sort
-        if ((values.length / 4) % 1 === 0) {//find quartiles
-            q1 = 1 / 2 * (values[(values.length / 4)] + values[(values.length / 4) + 1]);
-            q3 = 1 / 2 * (values[(values.length * (3 / 4))] + values[(values.length * (3 / 4)) + 1]);
-        } else {
-            q1 = values[Math.floor(values.length / 4 + 1)];
-            q3 = values[Math.ceil(values.length * (3 / 4) + 1)];
-        }
-        iqr = q3 - q1;
-        maxValue = q3 + iqr * 1.5;
-        minValue = q1 - iqr * 1.5;
-        return values.filter((x) => (x >= minValue) && (x <= maxValue));
+        return Math.round(sumOfValues / values.length);
     }
 }
